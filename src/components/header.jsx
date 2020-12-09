87f798029d583df9bc6fb12014fd23a6e4e8fcb1
MXcray <urazaev.tosha@yandex.ru>
2020-12-10 02:09:22 +0400

    base

---
+++ src/components/header.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+    return (
+    <header className="header">
+        <div className="container">
+            <div className="logo">
+                <img src="https://cdn.auth0.com/blog/react-js/react.png" />
+                <h3 className="title header__title">My <span> React</span> project</h3>
+            </div>
+        </div>
+    </header>
+    );
+}
+
+export default Header;
---
+++ src/components/nav.jsx
@@ -0,0 +1,12 @@
+const Nav = () => {
+    return (
+    <nav className="nav">
+        <a href="#">1</a>
+        <a href="#">2</a>
+        <a href="#">3</a>
+        <a href="#">4</a>
+    </nav>
+    );
+}
+
+export default Nav;
